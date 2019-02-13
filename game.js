fc6c4b33d7394dac3d0401f603386414e91d5e5e
Raul Delgado <raul@MacBook-Pro-de-Raul.local>
2019-02-13 14:43:52 +0100

    game logic done

--- game.js
+++ game.js
@@ -1,1 +1,37 @@
 'use strict'
+
+class Game{
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.ctx = this.canvas.getContext('2d');
+        this.player;
+        this.enemies = [];
+    };
+
+    startLoop() {
+        
+        const loop = () => {
+            
+            this.updateCanvas();
+            this.clearCanvas();
+            this.drawCAnvas();
+            window.requestAnimationFrame(loop);
+        }
+
+        window.requestAnimationFrame(loop);
+    }
+    
+    updateCanvas() {
+
+    };
+
+    clearCanvas() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    };
+
+    drawCAnvas() {
+
+    };
+
+
+}
